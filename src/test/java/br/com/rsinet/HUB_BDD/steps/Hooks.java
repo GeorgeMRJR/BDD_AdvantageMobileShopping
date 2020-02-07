0241fbe6c014bd16b6944421e3b4cd0e00b4ea7b
George <georgeluiz.ti@gmail.com>
2020-02-07 15:42:26 -0300

    gitignore

--- src/test/java/br/com/rsinet/HUB_BDD/steps/Hooks.java
+++ src/test/java/br/com/rsinet/HUB_BDD/steps/Hooks.java
@@ -2,15 +2,12 @@ package br.com.rsinet.HUB_BDD.steps;
 
 import java.net.MalformedURLException;
 
-import org.junit.AfterClass;
 import org.openqa.selenium.WebDriver;
 
 import br.com.rsinet.HUB_BDD.suporte.Driver;
 import br.com.rsinet.HUB_BDD.suporte.Screenshot;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
 
 public class Hooks {
 	private WebDriver driver;
@@ -24,21 +21,21 @@ public class Hooks {
 
 	@After(order = 0)
 	public void resetApp() throws MalformedURLException, InterruptedException {
-		AndroidDriver<MobileElement> androidDriver = Driver.getDriver();
-		try {
-			androidDriver.resetApp();
-		} catch (Exception e) {
-			Driver.fecharDriver();
-			System.out.println(e.getMessage());
-		}
-
-		Thread.sleep(1000);
-//		Driver.fecharDriver();
-	}
-
-	@AfterClass
-	public void tearDown() {
+//		AndroidDriver<MobileElement> androidDriver = Driver.getDriver();
+//		try {
+//			androidDriver.resetApp();
+//		} catch (Exception e) {
+//			Driver.fecharDriver();
+//			System.out.println(e.getMessage());
+//		}
+//
+//		Thread.sleep(1000);
 		Driver.fecharDriver();
-
 	}
+
+//	@AfterClass
+//	public void tearDown() {
+//		Driver.fecharDriver();
+//
+//	}
 }
